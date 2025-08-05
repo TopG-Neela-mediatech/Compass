e447e3ac58113d334972322098bcb42387897fc1
Chet2k <chetanr@neelafilm.com>
2025-08-05 17:38:45 +0530

    w

--- Assets/Scripts/LevelScripts/LevelManager.cs
+++ Assets/Scripts/LevelScripts/LevelManager.cs
@@ -1,9 +1,20 @@
+using DG.Tweening;
 using UnityEngine;
 
 namespace TMKOC.Compass
 {
     public class LevelManager : MonoBehaviour
     {
-       
+        [SerializeField] private Transform buttonParentT;
+
+
+        private void Start()
+        {
+            MoveButtonsOut();
+        }
+        private void MoveButtonsOut()
+        {
+            buttonParentT.DOLocalMoveY(-500f, 0f);
+        }
     }
 }
