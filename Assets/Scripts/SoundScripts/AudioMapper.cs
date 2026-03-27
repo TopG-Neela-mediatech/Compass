277b3bc43e3c4eb4275c0734fd084de8c180cc56
Chet2k <chetanr@neelafilm.com>
2026-03-27 11:38:04 +0530

    made ab

---
+++ Assets/Scripts/SoundScripts/AudioMapper.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace TMKOC.Compass
+{
+    public class AudioMapper : MonoBehaviour
+    {
+        public string gameIntro;
+        public string gameOutro;
+        public string[] directionAudio;
+        public string[] genericQuestionAudio;
+    }
+}
--- Assets/Scripts/SoundScripts/SoundManager.cs
+++ Assets/Scripts/SoundScripts/SoundManager.cs
@@ -5,7 +5,7 @@ namespace TMKOC.Compass
 {
     public class SoundManager : MonoBehaviour
     {
-        [SerializeField] private SoundSO englishSO;
+        /*[SerializeField] private SoundSO englishSO;
         [SerializeField] private SoundSO hindiSO;
         [SerializeField] private SoundSO tamilSO;
         [SerializeField] private SoundSO frenchSO;
@@ -15,103 +15,108 @@ namespace TMKOC.Compass
         [SerializeField] private SoundSO bengaliSO;
         [SerializeField] private AudioSource levelAudioSource;
         [SerializeField] private string audioLocalization;
-        private SoundSO soundData;
+        private SoundSO soundData;*/
+        [SerializeField] private AudioMapper audioMapper;
 
 
         private void Awake()
         {
-            SetLanguage();
+           // SetLanguage();
         }
         private void Start()
         {
             GameManager.Instance.OnLevelStart += PlayGenericQuestions;
             GameManager.Instance.OnLevelLose += PlayRetryAudio;
         }
-        private void PlayLevelAudio(AudioClip clip)
-        {
-            if (clip != null)
-            {
-                levelAudioSource.PlayOneShot(clip);
-            }
-        }
+        /* private void PlayLevelAudio(AudioClip clip)
+         {
+             if (clip != null)
+             {
+                 levelAudioSource.PlayOneShot(clip);
+             }
+         }
+         private void SetLanguage()
+         {
+             audioLocalization = PlayerPrefs.GetString("PlayschoolLanguageAudio", audioLocalization);
+             switch (audioLocalization)
+             {
+                 case "English":
+                     soundData = englishSO;
+                     break;
+                 case "EnglishUS":
+                     soundData = englishSO;
+                     break;
+                 case "Hindi":
+                     soundData = hindiSO;
+                     break;
+                 case "Tamil":
+                     soundData = tamilSO;
+                     break;
+                 case "French":
+                     soundData = frenchSO;
+                     break;
+                 case "Punjabi":
+                     soundData = punjabiSO;
+                     break;
+                 case "Malayalam":
+                     soundData = malayalamSO;
+                     break;
+                 case "Marathi":
+                     soundData = marathiSO;
+                     break;
+                 case "Bengali":
+                     soundData = bengaliSO;
+                     break;
+                 default:
+                     soundData = englishSO;
+                     break;
+             }
+         }
+        */
         public float PlayIntro()//for level one play intro/welcome audio
         {
-            if (levelAudioSource.isPlaying) { levelAudioSource.Stop(); }
-            PlayLevelAudio(soundData.gameIntro);
-            return soundData.gameIntro.length;
+           // if (levelAudioSource.isPlaying) { levelAudioSource.Stop(); }
+            float f = RuntimeAudioLoader.Instance.PlayRuntimeAudio(audioMapper.gameIntro);
+            return f;
         }
         public void PlayOutro()//for level one play intro/welcome audio
         {
-            if (levelAudioSource.isPlaying) { levelAudioSource.Stop(); }
-            PlayLevelAudio(soundData.gameOutro);
+           // if (levelAudioSource.isPlaying) { levelAudioSource.Stop(); }
+            RuntimeAudioLoader.Instance.PlayRuntimeAudio(audioMapper.gameOutro);
         }
         public float PlayFlashCardAudio(int index)
         {
-            if (levelAudioSource.isPlaying) { levelAudioSource.Stop(); }
-            PlayLevelAudio(soundData.directionAudio[index]);
-            return soundData.directionAudio[index].length;
+            //if (levelAudioSource.isPlaying) { levelAudioSource.Stop(); }
+            float f = RuntimeAudioLoader.Instance.PlayRuntimeAudio(audioMapper.directionAudio[index]);
+            return f;
         }
         private void PlayGenericQuestions()
         {
-            if (levelAudioSource.isPlaying) { levelAudioSource.Stop(); }
-            int rand = UnityEngine.Random.Range(0, soundData.genericQuestionAudio.Length);
-            PlayLevelAudio(soundData.genericQuestionAudio[rand]);
+           // if (levelAudioSource.isPlaying) { levelAudioSource.Stop(); }
+            int rand = UnityEngine.Random.Range(0, audioMapper.genericQuestionAudio.Length);
+            float f = RuntimeAudioLoader.Instance.PlayRuntimeAudio(audioMapper.genericQuestionAudio[rand]);
         }
         public void PlayCorrectAudio()
         {
-            if (levelAudioSource.isPlaying) { levelAudioSource.Stop(); }
-            int rand = UnityEngine.Random.Range(0, soundData.correctAudio.Length);
-            PlayLevelAudio(soundData.correctAudio[rand]);
+            /* if (levelAudioSource.isPlaying) { levelAudioSource.Stop(); }
+             int rand = UnityEngine.Random.Range(0, soundData.correctAudio.Length);
+             PlayLevelAudio(soundData.correctAudio[rand]);*/
+            RuntimeAudioLoader.Instance.PlayCorrectAudioClip();
         }
         public void PlayInCorrectAudio()
         {
-            if (levelAudioSource.isPlaying) { levelAudioSource.Stop(); }
-            int rand = UnityEngine.Random.Range(0, soundData.incorrectAudio.Length);
-            PlayLevelAudio(soundData.incorrectAudio[rand]);
+            /* if (levelAudioSource.isPlaying) { levelAudioSource.Stop(); }
+             int rand = UnityEngine.Random.Range(0, soundData.incorrectAudio.Length);
+             PlayLevelAudio(soundData.incorrectAudio[rand]);*/
+            RuntimeAudioLoader.Instance.PlayIncorrectAudioClip();
         }
         private void PlayRetryAudio()
         {
-            if (levelAudioSource.isPlaying) { levelAudioSource.Stop(); }
+           /* if (levelAudioSource.isPlaying) { levelAudioSource.Stop(); }
             int rand = UnityEngine.Random.Range(0, soundData.retryAudio.Length);
-            PlayLevelAudio(soundData.retryAudio[rand]);
-        }
-        private void SetLanguage()
-        {
-            audioLocalization = PlayerPrefs.GetString("PlayschoolLanguageAudio", audioLocalization);
-            switch (audioLocalization)
-            {
-                case "English":
-                    soundData = englishSO;
-                    break;
-                case "EnglishUS":
-                    soundData = englishSO;
-                    break;
-                case "Hindi":
-                    soundData = hindiSO;
-                    break;
-                case "Tamil":
-                    soundData = tamilSO;
-                    break;
-                case "French":
-                    soundData = frenchSO;
-                    break;
-                case "Punjabi":
-                    soundData = punjabiSO;
-                    break;
-                case "Malayalam":
-                    soundData = malayalamSO;
-                    break;
-                case "Marathi":
-                    soundData = marathiSO;
-                    break;
-                case "Bengali":
-                    soundData = bengaliSO;
-                    break;
-                default:
-                    soundData = englishSO;
-                    break;
-            }
-        }
+            PlayLevelAudio(soundData.retryAudio[rand]);*/
+           RuntimeAudioLoader.Instance.PlayRetryAudioClip();
+        }       
         private void OnDestroy()
         {
             GameManager.Instance.OnLevelStart -= PlayGenericQuestions;
